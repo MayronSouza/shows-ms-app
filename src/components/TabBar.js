28154e6725f73c9272c3603a42baad80cdca7dc3
MayronSouza <mayron.souza@gmz.solutions>
2021-04-04 16:56:58 -0300

    finalizing the TabBar component

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { View, StyleSheet, TouchableOpacity } from "react-native"
 import Feather from 'react-native-vector-icons/Feather'
 
-const TabBar = ({ navigation }) => {
+const TabBar = ({ navigation, state }) => {
   return (
     <View style={styles.tab}>
       <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-        <Feather name="home" size={32} color="white" />
+        <Feather
+          name="home"
+          size={32}
+          color={state.index === 0 ? "white" : "black"}
+        />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate('Shows')}>
-        <Feather name="list" size={32} color="white" />
+        <Feather
+          name="list"
+          size={32}
+          color={state.index === 1 ? "white" : "black"}
+        />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate('NewShow')}>
-        <Feather name="plus-circle" size={32} color="white" />
+        <Feather
+          name="plus-circle"
+          size={32}
+          color={state.index === 2 ? "white" : "black"}
+        />
       </TouchableOpacity>
     </View>
   );
