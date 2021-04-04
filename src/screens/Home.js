a7d829df52b3210d45c77482a29b81d3e27edb5b
MayronSouza <mayron.souza@gmz.solutions>
2021-04-04 16:20:52 -0300

    structuring, the Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 
-const Home = () => {
+const Home = ({ navigation }) => {
+
   return (
-    <View style={styles.mainView}>
-      <Text>Hello World in the Home Screen!</Text>
+    <View>
+      <Text>Home</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  mainView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-});
 
 export { Home };
