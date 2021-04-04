003aea32b1f6859dc96b2bdc5de51275ee0517da
MayronSouza <mayron.souza@gmz.solutions>
2021-04-04 16:21:53 -0300

    structuring, the NewShow screen

---
+++ src/screens/NewShow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const NewShow = () => {
+  return (
+    <View>
+     <Text>New Show</Text>
+    </View>
+  );
+}
+
+export { NewShow };
