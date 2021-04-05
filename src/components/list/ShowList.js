ae5b36bc85d0d2374aea0bb569e55f1857e97e96
MayronSouza <mayron.souza@gmz.solutions>
2021-04-05 11:44:49 -0300

    inserting the FlatList in ShowList component

--- src/components/list/ShowList.js
+++ src/components/list/ShowList.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
+const items = [
+    {
+      id: 1,
+      name: 'Friends',
+    },
+    {
+      id: 2,
+      name: 'Heroes',
+    },
+    {
+      id: 3,
+      name: 'Two and half man',
+    },
+  ];
 const ShowList = () => {
   return (
-    <View>
-      <Text>ShowList</Text>
-    </View>
+    <FlatList
+      style={styles.container}
+      data={items}
+      renderItem={
+        ({ item }) => (
+          <TouchableOpacity onPress={() => console.log(item.id)} style={styles.buttonTouch}>
+            <Text style={styles.text}>{item.name}</Text>
+          </TouchableOpacity>
+        )
+      }
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+  },
+  buttonTouch: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 8,
+    padding: 5,
+    borderRadius: 50,
+    backgroundColor: '#f99000'
+  },
+  text: {
+    padding: 5,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
+  }
+});
 
 export { ShowList };
