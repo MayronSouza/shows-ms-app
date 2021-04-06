b2d3b6b461ee07132baa0a53abe3aa0a082b1c25
MayronSouza <mayron.souza@gmz.solutions>
2021-04-06 16:58:20 -0300

    inserting the Context into the App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -6,18 +6,23 @@ import { Home } from './screens/Home';
 import { Shows } from './screens/Shows';
 import { NewShow } from './screens/NewShow';
 import { TabBar } from './components/tab/TabBar';
+import { AppContext, appReducer, initialState } from './context/AppContext';
 
 const tabNav = createBottomTabNavigator();
 
 const App = () => {
+  const [state, dispatch] = useReducer(appReducer, initialState);
+
   return (
-    <NavigationContainer>
+    <AppContext.Provider value={{ state, dispatch }}>
+      <NavigationContainer>
       <tabNav.Navigator tabBar={props => <TabBar {...props} />}>
         <tabNav.Screen name={'Home'} component={Home} />
         <tabNav.Screen name={'Shows'} component={Shows} />
         <tabNav.Screen name={'NewShow'} component={NewShow} />
       </tabNav.Navigator>
     </NavigationContainer>
+    </AppContext.Provider>
   );
 }
 
